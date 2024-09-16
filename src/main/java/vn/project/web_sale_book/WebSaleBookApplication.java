7f70368fec1dc54b5af626f0a36665cec67c8f21
ThanhLinh0704 <lethanhlinh0704@gmail.com>
2024-09-16 13:25:36 +0700

    upload code

---
+++ src/main/java/vn/project/web_sale_book/WebSaleBookApplication.java
@@ -0,0 +1,13 @@
+package vn.project.web_sale_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSaleBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSaleBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/project/web_sale_book/WebSaleBookApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.project.web_sale_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSaleBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
